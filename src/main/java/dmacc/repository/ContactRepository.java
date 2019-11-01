e9b332c36637651e3540fc8628adedb39b9eda0e
Robert Hoehle <robert.hoehle@gmail.com>
2019-10-31 22:31:39 -0500

    First commit

---
+++ src/main/java/dmacc/repository/ContactRepository.java
@@ -0,0 +1,7 @@
+package dmacc.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import dmacc.beans.Contact;
+
+public interface ContactRepository extends JpaRepository<Contact, Long> { }
